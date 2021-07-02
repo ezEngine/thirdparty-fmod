21a57c5014feeb2d429194233339f69638c1017d
Jan Krassnigg <Jan@Krassnigg.de>
2021-07-02 13:48:42 +0200

    Moved CMake files here

---
+++ init.cmake
@@ -0,0 +1,37 @@
+######################################
+### Fmod support
+######################################
+
+set (EZ_BUILD_FMOD ON CACHE BOOL "Whether support for FMOD should be added")
+
+######################################
+### ez_requires_fmod()
+######################################
+
+macro(ez_requires_fmod)
+
+	ez_requires_windows()
+	ez_requires(EZ_BUILD_FMOD)
+
+endmacro()
+
+######################################
+### ez_link_target_fmod(<target>)
+######################################
+
+function(ez_link_target_fmod TARGET_NAME)
+
+	ez_requires_fmod()
+
+	if (EZFMOD_FOUND)
+        target_link_libraries(${TARGET_NAME} PRIVATE ezFmod::Studio)
+
+        add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:ezFmod::Studio> $<TARGET_FILE_DIR:${TARGET_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:ezFmod::Core> $<TARGET_FILE_DIR:${TARGET_NAME}>
+	  )
+
+	endif()
+
+endfunction()
+
